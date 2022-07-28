27ba2790d539874365c884e595615f0b4d01e029
Uladzislau Zhaludkovich <zhaludkovichuladzislau@gmail.com>
2022-07-28 08:53:36 +0000

    Update base.py

--- application/mysite/mysite/settings/base.py
+++ application/mysite/mysite/settings/base.py
@@ -88,8 +88,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'wagtail',
-        'USER': 'wagtail',
-        'PASSWORD': 'MWYyZDFlMmU2N2Rm',
+        'USER': db-user-pass,
+        'PASSWORD': db-user-pass,
         'HOST': '10.72.2.244',
         'PORT': '5432',
     }
